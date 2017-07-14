3fd8231b062acf56a4304349056eeb50e6041a12
gitte vermeiren <gitte.vermeiren@gmail.com>
2017-07-14 20:17:54 +0000

    added post

--- src/TodoMvcAPI/index.js
+++ src/TodoMvcAPI/index.js
@@ -1,12 +1,12 @@
 var { graphql, buildSchema } = require('graphql');
 var Schema = require('./schema');
 
-var query = context.req.body.query;// 'query { todos { id, title, completed } }';
 var mutation = 'mutation {  add(title: "Read a book") {id,title}}';
 
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    
+    var query = context.req.body.query;// 'query { todos { id, title, completed } }';
+   
     graphql(Schema, query)
         .then((response) => {
             context.log(response);
