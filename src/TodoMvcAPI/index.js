e7b27a17a0d5ca1f268fb90cc37b87823231f768
gitte vermeiren <gitte.vermeiren@gmail.com>
2017-07-14 12:45:33 +0000

    added graphql query

--- src/TodoMvcAPI/index.js
+++ src/TodoMvcAPI/index.js
@@ -1,8 +1,29 @@
+var { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+// graphql(schema, '{ hello }', root).then((response) => {
+//   console.log(response);
+// });
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "Hello " + req.params.name
+        body: graphql(schema, req.params.name, root)
     };
     context.done(); 
 };
