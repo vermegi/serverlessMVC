3a89ead3fbefe4fc709cc2b46c824531f757522c
gitte vermeiren <gitte.vermeiren@gmail.com>
2017-07-14 20:38:14 +0000

    added second react function

--- src/ToMVCReact/index.js
+++ src/ToMVCReact/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request for REACT.');
