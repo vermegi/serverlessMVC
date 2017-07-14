e0bb1cc3cdda48b821fee94b63b1daec15ee5d6a
gitte vermeiren <gitte.vermeiren@gmail.com>
2017-07-14 12:57:22 +0000

    I suck at JS

--- src/TodoMvcAPI/index.js
+++ src/TodoMvcAPI/index.js
@@ -23,7 +23,7 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: graphql(schema, '{ hello }' , root).then((response) => context.log(response)) //req.params.name
+        body: graphql(schema, '{ hello }' , root).then((response) => {return response;}) //req.params.name
     };
     context.done(); 
 };
